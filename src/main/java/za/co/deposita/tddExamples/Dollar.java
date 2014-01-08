1a15b0af4828a118ab5ba226b73c4d1840d95cdf
sollymcs <solly.mogashane@deposita.co.za>
2014-01-07 23:15:37 -0800

    TDD Examples

---
+++ src/main/java/za/co/deposita/tddExamples/Dollar.java
@@ -0,0 +1,11 @@
+package za.co.deposita.tddExamples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Solly
+ * Date: 2014/01/06
+ * Time: 9:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dollar {
+}
---
+++ src/test/java/za/co/deposita/tddExamples/equalityTest.java
@@ -0,0 +1,11 @@
+package za.co.deposita.tddExamples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Solly
+ * Date: 2014/01/07
+ * Time: 10:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class equalityTest {
+}
